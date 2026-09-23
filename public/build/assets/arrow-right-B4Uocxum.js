6d9ca5024625dbe05f625e139f4c58c48ef5a844
Jay <jaynptl21@gmail.com>
2026-09-23 15:52:25 +0530

    Build included in commits

---
+++ public/build/assets/arrow-right-B4Uocxum.js
@@ -0,0 +1,1 @@
+import{n as e}from"./mail-C2KpL2Z9.js";var t={name:`arrow-right`,size:24,node:[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`m12 5 7 7-7 7`,key:`xquz4c`}]]};t.node;var n=e(t);export{n as t};
